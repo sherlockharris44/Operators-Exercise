82c0f21c7207fa35a91c6cae5bcc27bacf01a672
James Harris <sherlockharris44@gmail.com>
2026-04-06 22:09:52 -0700

    Completed Exercies 1 & 2 for OperatorsExercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            Console.WriteLine($"Please enter radius of Circle");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The Area of Circle with the radius of {radius} is: {AreaOfCircle(radius)}");
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI* Math.Pow(radius, 2);
+        }
+
+        
     }
 }
